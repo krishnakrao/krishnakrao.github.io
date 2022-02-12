c5f792b13305a7b1f51b5aeda258b3f6be7133e9
krishnakrao <kkrao.j@gmail.com>
2022-02-12 02:12:25 -0800

    updates

--- _pages/script2.js
+++ _pages/script2.js
@@ -1,5 +1,5 @@
 var data = {rss: "https://medium.com/feed/@kkrao"}
-$.get(https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kkrao, data, funciton(response){
+$.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kkrao', data, function(response){
 //Checking Response Status
 if (response.status == 'ok') {
    //Define a variable
