d710d1f1d6b3ab567b5fd9cff9b77e4b3b79ad23
krishnakrao <kkrao.j@gmail.com>
2022-02-12 02:09:57 -0800

    updates

--- _pages/script.js
+++ _pages/script.js
@@ -2,7 +2,7 @@ $(function () {
             var mediumPromise = new Promise(function (resolve) {
             var $content = $('#jsonContent');
             var data = {
-                rss: 'https://medium.com/feed/@sabesan96'
+                rss: 'https://medium.com/feed/@kkrao'
             };
             $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kkrao', data, function (response) {
                 if (response.status == 'ok') {
